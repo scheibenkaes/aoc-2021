947601f4ce47f98743f3e97a960336a6e6234361
Benjamin KlÃ¼glein <scheibenkaes@gmail.com>
2021-12-01 12:06:30 +0100

    simple solution with intermediate results

--- src/aoc/day1.clj
+++ src/aoc/day1.clj
@@ -2,12 +2,25 @@
   (:require [aoc.core :as aoc]))
 
 (defn solve
-  ""
   [scans]
   (reduce (fn [acc n]
-          (if (empty? acc)
-            [[n nil]]
-            (let [[n-prev _] (last acc)]
-              (if (> n-prev n)
-                (conj acc [n :decreased])
-                (conj acc [n :increased]))))) [] scans))
+            (if (empty? acc)
+              [[n nil]]
+              (let [[n-prev _] (last acc)]
+                (if (> n-prev n)
+                  (conj acc [n :decreased])
+                  (conj acc [n :increased]))))) [] scans))
+
+(defn intermediates-to-result
+  [intermediate]
+  (->> intermediate
+       (map second)
+       (filter #(= :increased %))
+       (count)))
+
+(defn run-on-test-file!
+  []
+  (->> 1
+       aoc/load-input-as-numbers
+       solve
+       intermediates-to-result))
