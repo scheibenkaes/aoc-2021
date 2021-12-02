78b711720be8d179127844de831345205377c67b
Benjamin Klueglein <scheibenkaes@gmail.com>
2021-12-02 14:16:36 +0100

    generalize loading

--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -9,18 +9,21 @@
       (io/reader)
       line-seq))
 
+(defn load-input-with
+  "Load input for a given day and apply a line-transformer to each line."
+  [day line-transformer]
+  (->> day
+       load-input
+       (map line-transformer)))
+
 (defn load-input-as-numbers
   "Interpret input as numbers."
   [day]
-  (->> day
-       load-input
-       (map #(Integer/parseInt %))))
+  (load-input-with day #(Integer/parseInt %)))
 
 
 (defn load-input-as-instructions
   [day]
-  (->> day
-       load-input
-       (mapv (fn [line]
-               (let [[dir amount] (clojure.string/split line #" ")]
-                [(keyword dir) (Integer/parseInt amount)])))))
+  (load-input-with day (fn [line]
+                         (let [[dir amount] (clojure.string/split line #" ")]
+                           [(keyword dir) (Integer/parseInt amount)]))))
