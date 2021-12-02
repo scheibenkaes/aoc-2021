15276c8041fa4f45563b1c193f2f8fbda9a5dcae
Benjamin Klueglein <scheibenkaes@gmail.com>
2021-12-02 13:12:26 +0100

    add pt2
    reuse code by handing it a interpreter multimethod

--- src/aoc/day2.clj
+++ src/aoc/day2.clj
@@ -1,9 +1,10 @@
 (ns aoc.day2
   (:require [aoc.core :as aoc]))
 
-(defonce ship-data {:horizontal   0
-                    :depth        0
-                    :instructions []})
+(def ship-data {:aim          0
+                :horizontal   0
+                :depth        0
+                :instructions []})
 
 (defmulti apply-instruction (fn [inst ship] (first inst)))
 
@@ -24,11 +25,11 @@
   (assoc ship :mult (* (get ship :depth 0) (get ship :horizontal 0))))
 
 (defn solve
-  [{:keys [instructions] :as ship}]
+  [interpreter {:keys [instructions] :as ship}]
   (if-let [inst (first instructions)]
     (let [remaining (drop 1 instructions)
-          ship*     (apply-instruction inst ship)]
-      (recur (assoc ship* :instructions remaining)))
+          ship*     (interpreter inst ship)]
+      (recur interpreter (assoc ship* :instructions remaining)))
     (calc-mult ship)))
 
 (defn prepare-ship
@@ -40,6 +41,33 @@
   (->> 2
        aoc/load-input-as-instructions
        prepare-ship
-       solve))
+       (solve apply-instruction)))
 
 ;; ----------------- pt 2
+
+(defmulti apply-instruction2 (fn [inst ship] (first inst)))
+
+(defmethod apply-instruction2 :forward
+  [[_ value] ship]
+  (-> ship
+      (update-in [:horizontal] + value)
+      (update-in [:depth] + (* value (:aim ship)) )))
+
+(defmethod apply-instruction2 :up
+  [[_ value] ship]
+  (update-in ship [:aim] - value))
+
+(defmethod apply-instruction2 :down
+  [[_ value] ship]
+  (update-in ship [:aim] + value))
+
+(defn pt2-solve
+  [instructions]
+  (let []
+    (solve apply-instruction2 (prepare-ship instructions))))
+
+(defn run-pt2!
+  ""
+  []
+  (let [instructions (aoc/load-input-as-instructions 2)]
+    (pt2-solve instructions)))
--- test/aoc/day2_test.clj
+++ test/aoc/day2_test.clj
@@ -11,7 +11,7 @@
 
 (deftest solve-test
   (testing "test input leads to output"
-    (let [{:keys [depth horizontal mult]} (sut/solve (sut/prepare-ship test-input))]
+    (let [{:keys [depth horizontal mult]} (sut/solve sut/apply-instruction (sut/prepare-ship test-input))]
       (is (= mult 150))
       (is (= depth 10))
       (is (= horizontal 15)))))
@@ -19,3 +19,11 @@
 (deftest ^:integration solve-on-input-file
   (let [result (sut/run-on-test-file!)]
     (is (= 1855814 (:mult result)))))
+
+(deftest pt2-test
+  (let [result (sut/pt2-solve test-input)]
+    (is (= (:mult result) 900))))
+
+(deftest ^:integration run-pt2-test
+  (let [result (sut/run-pt2!)]
+    (is (= 1845455714 (:mult result)))))
