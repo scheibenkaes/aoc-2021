1fea45ef9c77a711373d383d576b0981b4b0ed8d
Benjamin Klueglein <scheibenkaes@gmail.com>
2021-12-03 11:28:23 +0100

    pt deux

--- src/aoc/day3.clj
+++ src/aoc/day3.clj
@@ -1,16 +1,20 @@
 (ns aoc.day3
   (:require [aoc.core :as aoc]))
 
+(defn occurences
+  [line]
+  (reduce (fn [acc c] (update-in acc [c] inc)) {\0 0 \1 0} line))
+
 (defn calc-rate
   [rate line]
   (->> line
-       (reduce (fn [acc c] (update-in acc [c] inc)) {\0 0 \1 0})
+       occurences
        (sort-by val rate)
        ffirst))
 
 (def epsilon (partial calc-rate <))
 
-(def gamma (partial calc-rate >))
+(def gamma (partial calc-rate >=))
 
 (defn take-bit-at-pos
   [lines pos]
@@ -29,17 +33,42 @@
     (clojure.string/join cs)
     (Integer/parseInt cs 2)))
 
+;; pt 2
+
+(defn oxygen
+  ([lines] (oxygen lines 0))
+  ([lines bit-pos]
+   (let [fst (take-bit-at-pos lines bit-pos)
+         sig (gamma fst)
+         w1  (filter #(= (get % bit-pos) sig) lines)]
+     (if (= (count w1) 1)
+       (Integer/parseInt (first w1) 2)
+       (recur w1 (inc bit-pos))))))
+
+(defn co2
+  ([lines] (co2 lines 0))
+  ([lines bit-pos]
+   (let [fst (take-bit-at-pos lines bit-pos)
+         sig (epsilon fst)
+         w1  (filter #(= (get % bit-pos) sig) lines)]
+     (if (= (count w1) 1)
+       (Integer/parseInt (first w1) 2)
+       (recur w1 (inc bit-pos))))))
+
 (defn solve
   [nums]
-  (let [bits (to-nth-bits nums)
-        eps  (-> (map epsilon bits)
-                 (char-seq->int))
-        gm   (-> (map gamma bits)
-                 (char-seq->int))]
-    {:epsilon eps
-     :gamma   gm
-     :bits    bits
-     :mult    (* eps gm)}))
+  (let [bits    (to-nth-bits nums)
+        eps     (-> (map epsilon bits)
+                    (char-seq->int))
+        gm      (-> (map gamma bits)
+                    (char-seq->int))
+        oxygen* (oxygen nums)
+        co2*    (co2 nums)]
+    {:epsilon      eps
+     :gamma        gm
+     :bits         bits
+     :mult         (* eps gm)
+     :life-support (* co2* oxygen*)}))
 
 (defn run-on-test-data!
   []
--- test/aoc/day3_test.clj
+++ test/aoc/day3_test.clj
@@ -15,15 +15,30 @@
                  "00010"
                  "01010"])
 
+(deftest oxgen-test
+  (is (= 23 (sut/oxygen test-input))))
+
+(deftest co2-test
+  (is (= 10 (sut/co2 test-input))))
+
+(deftest epsilon-test
+  (testing "with same number of bits returns 0"
+    (is (= (sut/epsilon "10") (sut/epsilon "010101") \0))))
+
+(deftest gamma-test
+  (testing "with same number of bits returns 1"
+    (is (= (sut/gamma "10") (sut/gamma "101010") \1))))
 
 (deftest solve-test
   (testing "test input leads to output"
-    (let [{gamma :gamma epsilon :epsilon bits :bits mult :mult} (sut/solve test-input)]
+    (let [{gamma :gamma epsilon :epsilon bits :bits mult :mult life-support :life-support} (sut/solve test-input)]
       (is (not (nil? bits)))
       (is (= gamma 22))
       (is (= epsilon 9))
-      (is (= mult 198)))))
+      (is (= mult 198))
+      (is (= life-support 230)))))
 
 (deftest ^:integration solve-on-input-file
   (let [result (sut/run-on-test-data!)]
-    (is (= 3882564 (:mult result)))))
+    (is (= 3882564 (:mult result)))
+    (is (= 3385170 (:life-support result)))))
