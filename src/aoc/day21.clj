80ce1b64e47e73b98eec9ab7aff084cf6c1c0cab
Benjamin Klueglein <scheibenkaes@gmail.com>
2021-12-21 17:41:17 +0100

    computer sais too low

--- src/aoc/day21.clj
+++ src/aoc/day21.clj
@@ -8,3 +8,73 @@
                 second
                 aoc/->int))]
     [(read-pos p1) (read-pos p2)]))
+
+(defn ->player
+  ""
+  [start]
+  start)
+
+(defn ->board
+  ""
+  []
+  (cycle (range 1 11)))
+
+(defn ->dice
+  ""
+  []
+  (let [nums (cycle (range 1 101))]
+    {:nums         nums
+     :times-rolled 0
+     :roll         nil}))
+
+(defn ->game
+  ""
+  [lines]
+  (let [[p1 p2] (load-starting-pos lines)]
+    {:player1 (->player p1)
+     :player2 (->player p2)
+     :score1  0
+     :score2  0
+     :board   (->board)
+     :dice    (->dice)
+     :winner  nil}))
+
+(defn roll!
+  [{:keys [nums times-rolled] :as dice}]
+  (let [roll (first nums)]
+    (assoc dice
+           :roll roll
+           :times-rolled (inc times-rolled)
+           :nums (rest nums))))
+
+(defn tick
+  ""
+  [{:keys [player1 player2 dice board] :as game}]
+  (letfn [(score-player [game dice position score player]
+            (let [board* (drop-while #(not= position %) board)
+                  roll1  (roll! dice)
+                  roll2  (roll! roll1)
+                  roll3  (roll! roll2)
+                  rolls  (map :roll [roll1 roll2 roll3])
+                  rolls  (apply + rolls)
+                  points (nth board* rolls)]
+              (-> game
+                  (update score + points)
+                  (assoc player points)
+                  (assoc :dice roll3))))]
+    (let [game1 (score-player game dice player1 :score1 :player1)
+          game2 (score-player game1 (:dice game1) player2 :score2 :player2)
+          winner (cond
+                   (>= (:score1 game1) 1000) :player1
+                   (>= (:score2 game2) 1000) :player2
+                   :else nil)]
+      (if winner
+        (assoc game1 :winner winner)
+        (assoc game2 :winner winner)))))
+
+(defn score-game
+  [{:keys [winner score1 score2 dice]}]
+  (when winner
+    (let [mn    (min score1 score2)
+          rolls (:times-rolled dice)]
+      (* mn rolls))))
--- test/aoc/day21_test.clj
+++ test/aoc/day21_test.clj
@@ -8,4 +8,59 @@ Player 2 starting position: 8"))
 
 (deftest load-start-pos-test
   (let [pos (sut/load-starting-pos test-data)]
-    (is (nil? pos))))
+    (is (= [4 8] pos))))
+
+(deftest roll!-test
+  (let [d (sut/->dice)
+        r (sut/roll! d)
+        rolls (iterate sut/roll! d)
+        rolls (nth rolls 5)
+
+        hundreds (iterate sut/roll! (sut/->dice))
+        hundreds (nth hundreds 100)
+
+        and1 (iterate sut/roll! (sut/->dice))
+        and1 (nth and1 101)]
+    (is (= 1 (:times-rolled r))
+        (= 1 (:roll r)))
+    (is (= 5 (:times-rolled rolls))
+        (= 5 (:roll rolls)))
+    (is (= 100 (:times-rolled hundreds))
+        (= 100 (:roll hundreds)))
+    (is (= 101 (:times-rolled and1))
+        (= 1   (:roll and1)))))
+
+(deftest tick-test
+  (let [game  (sut/->game test-data)
+        game* (sut/tick game)]
+    (is (= 10 (:score1 game*)))
+    (is (= 10 (:player1 game*)))
+    (is (= 3 (:score2 game*)))
+    (is (= 3 (:player2 game*)))
+    (is (nil? (:winner game*)))
+
+    (let [game* (sut/tick game*)]
+      (is (= 14 (:score1 game*)))
+      (is (= 4 (:player1 game*)))
+      (is (= 9 (:score2 game*)))
+      (is (= 6 (:player2 game*)))
+
+      (let [game* (sut/tick game*)]
+        (is (= 20 (:score1 game*)))
+        (is (= 6 (:player1 game*)))
+        (is (= 16 (:score2 game*)))
+        (is (= 7 (:player2 game*)))))))
+
+(deftest score-game-test
+  (let [game   (sut/->game test-data)
+        ticks  (iterate sut/tick game)
+        win    (first (filter #(not= nil (:winner %)) ticks))
+        result (sut/score-game win)]
+    (is (= 739785 result))))
+
+(deftest file-test
+  (let [game   (sut/->game (aoc/load-input 21))
+        ticks  (iterate sut/tick game)
+        win    (first (filter #(not= nil (:winner %)) ticks))
+        result (sut/score-game win)]
+    (is (= 862110 result))))
